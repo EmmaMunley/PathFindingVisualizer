775bfd63398a191664dd92ee40138c2544ffb4a4
Emma Munley <emmamunley@gmail.com>
2019-11-18 13:51:41 -0500

    favicon and default board

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,7 +3,7 @@ export default {
     rows: 25,
     cols: 15,
     startCoord: { x: 0, y: 0 },
-    endCoord: { x: 0, y: 5 },
+    endCoord: { x: 10, y: 10 },
   },
   stepIntervals: {
     search: 30,
--- src/utils/CreateGrid.ts
+++ src/utils/CreateGrid.ts
@@ -35,6 +35,60 @@ function createGrid(
     }
     nodes.push(row);
   }
+
+  nodes[2][0].isWall = true;
+  nodes[3][0].isWall = true;
+  nodes[4][0].isWall = true;
+  nodes[5][0].isWall = true;
+  nodes[6][0].isWall = true;
+  nodes[7][0].isWall = true;
+
+  nodes[0][2].isWall = true;
+  nodes[1][2].isWall = true;
+  nodes[1][3].isWall = true;
+  nodes[1][4].isWall = true;
+  nodes[1][5].isWall = true;
+  nodes[1][6].isWall = true;
+  nodes[1][7].isWall = true;
+  nodes[2][7].isWall = true;
+  nodes[3][7].isWall = true;
+  nodes[4][7].isWall = true;
+  nodes[5][7].isWall = true;
+
+  nodes[6][2].weight = 2;
+  nodes[6][3].weight = 2;
+  nodes[6][4].weight = 2;
+  nodes[6][5].weight = 2;
+  nodes[6][6].weight = 2;
+  nodes[6][7].weight = 2;
+  nodes[6][8].weight = 2;
+  nodes[6][9].weight = 2;
+
+  nodes[2][2].weight = 2;
+  nodes[2][3].weight = 2;
+  nodes[2][4].weight = 2;
+  nodes[2][5].weight = 2;
+  nodes[2][6].weight = 2;
+  nodes[2][7].weight = 2;
+  nodes[9][1].weight = 2;
+  nodes[8][1].weight = 2;
+  nodes[7][1].weight = 2;
+  nodes[6][1].weight = 2;
+
+  nodes[6][10].isWall = true;
+  nodes[7][10].isWall = true;
+  nodes[8][10].isWall = true;
+  nodes[9][10].isWall = true;
+  nodes[10][1].weight = 2;
+  nodes[10][2].weight = 2;
+  nodes[10][3].weight = 2;
+  nodes[10][4].weight = 2;
+  nodes[10][5].weight = 2;
+  nodes[10][6].weight = 2;
+  nodes[10][7].weight = 2;
+  nodes[10][8].weight = 2;
+  nodes[10][9].weight = 2;
+
   return { nodes, columnLength: COL, rowLength: ROW };
 }
 
